3059cbccca13d8514ed06673e69a716cf5488ce7
MikkoP <penttila.mikko@gmail.com>
2026-04-06 17:03:13 +0300

    chore: removed old comments

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,6 @@ use panic_halt as _;
 const PATTERN: [u8; 8] = euclidean_rhythm::<8>(3, 8);
 const TRIGGER_LENGTH_MS: u64 = 100;
 
-// const BEAT_OFF_MS: u64 = 200;
-// const REST_MS: u64 = 300;
-
 static TRIGGER_SIGNAL: Signal<CriticalSectionRawMutex, ()> = Signal::new();
 
 #[embassy_executor::task]
@@ -28,13 +25,6 @@ async fn trigger_task(mut clock_in: Input<'static>) {
         clock_in.wait_for_falling_edge().await;
     }
 }
-
-// async fn wait_or_signal(ms: u64) -> bool {
-//     match select(Timer::after_millis(ms), TRIGGER_SIGNAL.wait()).await {
-//         Either::First(_) => false,
-//         Either::Second(_) => true,
-//     }
-// }
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
@@ -63,28 +53,4 @@ async fn main(spawner: Spawner) {
             playhead = 0;
         }
     }
-
-    // This is the old logic where the trigger signal is used for play/stop
-    /*
-    loop {
-        TRIGGER_SIGNAL.wait().await;
-        TRIGGER_SIGNAL.reset();
-
-        'playing: for step in (0..PATTERN.len()).cycle() {
-            if PATTERN[step] == 1 {
-                led.set_high();
-                if wait_or_signal(BEAT_ON_MS).await {
-                    led.set_low();
-                    break 'playing;
-                }
-                led.set_low();
-                if wait_or_signal(BEAT_OFF_MS).await {
-                    break 'playing;
-                }
-            } else if wait_or_signal(REST_MS).await {
-                break 'playing;
-            }
-        }
-    }
-    */
 }
